3173eaa2b5c2403f760fa475171e11d5728c9e7a
Lucas Punchirolli <lucas@umaknow.com>
2019-10-29 23:54:37 -0400

    Intermediate refactor

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,27 +1,19 @@
-class Sorter {
-  constructor(public collection: number[]) {}
-  swap(arr: number[], n1: number, n2: number): void {
-    let tmp = arr[n1];
-    arr[n1] = arr[n2];
-    arr[n2] = tmp;
-  }
-  gnomeSort(): void {
-    for (let i = 0, arr = this.collection; i <= arr.length; ) {
-      if (i === 0) i++;
-      if (arr[i] >= arr[i - 1]) {
-        i++;
-      }
-      if (arr[i] < arr[i - 1]) {
-        this.swap(arr, i, i - 1);
-        i--;
-      }
-      if (i + 1 === arr.length) {
-        break;
-      }
-    }
-  }
-}
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
-const sorter = new Sorter([2, 1, 10, 2, 4, 90, 54, 32, 20, 77, 12]);
-sorter.gnomeSort();
-console.log(sorter.collection);
+const numbersCollection = new NumbersCollection([
+  2,
+  1,
+  10,
+  2,
+  4,
+  90,
+  54,
+  32,
+  20,
+  77,
+  12
+]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data);
