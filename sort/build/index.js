492da8102dcf7eb51cb510f2075c73ec0b314397
lcpunch <lucas@umaknow.com>
2020-02-06 13:00:21 -0500

    Implementing abstract classe instad of interface

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
 var CharactersCollection_1 = require("./CharactersCollection");
+var LinkedList_1 = require("./LinkedList");
 var numbersCollection = new NumbersCollection_1.NumbersCollection([
     2,
     1,
@@ -16,10 +16,14 @@ var numbersCollection = new NumbersCollection_1.NumbersCollection([
     77,
     12
 ]);
-var charactersCollection = new CharactersCollection_1.CharactersCollection('bbXXZsdasw');
-var sorter = new Sorter_1.Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
-var sorterChars = new Sorter_1.Sorter(charactersCollection);
-sorterChars.sort();
+var charactersCollection = new CharactersCollection_1.CharactersCollection('bbXXZsdasw');
+charactersCollection.sort();
 console.log(charactersCollection.data);
+var linkedList = new LinkedList_1.LinkedList();
+linkedList.add(90);
+linkedList.add(800);
+linkedList.add(1);
+linkedList.sort();
+linkedList.print();
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([
   2,
@@ -15,14 +16,18 @@ const numbersCollection = new NumbersCollection([
   77,
   12
 ]);
-
-const charactersCollection = new CharactersCollection('bbXXZsdasw');
-
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
-
-const sorterChars = new Sorter(charactersCollection);
-sorterChars.sort();
+const charactersCollection = new CharactersCollection('bbXXZsdasw');
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList()
+
+linkedList.add(90);
+linkedList.add(800);
+linkedList.add(1);
+
+linkedList.sort();
+linkedList.print();
