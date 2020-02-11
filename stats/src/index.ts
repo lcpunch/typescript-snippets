d8e58392a19a8b2d201cb263ddda5222682163e5
lcpunch <lucas@umaknow.com>
2020-02-11 13:14:26 -0500

    enum

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -8,4 +8,22 @@ const matches = fs.readFileSync('football.csv', {
   return row.split(',');
 });
 
-console.log(matches);
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+};
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+
+  // 
+  if(match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if(match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(manUnitedWins);
